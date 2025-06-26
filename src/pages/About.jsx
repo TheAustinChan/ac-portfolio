76732632add17f8b3aa91c65b558b89a8c617750
TheAustinChan <2012AustinChan2012@gmail.com>
2025-06-25 23:36:01 -0400

    First attempt to add pdf viewers

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,11 @@
 const About = () => {
-    return <h1>About Me!</h1>
+    return (<><h1>About Me!</h1>
+    <p>Welcome! I'm a new graduate from OSU, looking forward to creating exciting projects. 
+        Although I have my stronger and weaker domains, I consider myself a beginner in all, and passionate for all
+        domains of Software.
+        For more about my education and my skills, check out the home tab!
+    </p>
+    </>);
 };
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (<>
+  <h1>Home Page</h1>
+  <h2>Behold! My resume</h2>
+    <object data="/OSU%20Transcript%20(Graduated).pdf" type="application/pdf" width="100%" height="500px">
+      <p>Unable to display pdf <a href="/OSU%20Transcript%20(Graduated).pdf">Download Instead</a></p>
+    </object>
+  <h2>A transcript as well</h2>
+    <object data="/SWE%20Resume.pdf" type="application/pdf" width="100%" height="500px">
+      <p>Unable to display pdf <a href="/SWE%20Resume.pdf">Download Instead</a></p>
+    </object>
+  </>);
 };
 
 export default Home;
