c0ab160c387fdf7db23290fdd6a8a49cccabdba5
TheAustinChan <2012AustinChan2012@gmail.com>
2025-06-26 00:03:38 -0400

    ok so ghpages only works to show pdf if in exact subdirectory

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,10 @@ const Home = () => {
   return (<>
   <h1>Home Page</h1>
   <h2>Behold! My resume</h2>
-    <object data="OSUTranscript(Graduated).pdf" type="application/pdf" width="100%" height="500px">
-      <p>Unable to display pdf <a href="OSUTranscript(Graduated).pdf">Download Instead</a></p>
-    </object>
-  <h2>A transcript as well</h2>
     <object data="Resume.pdf" type="application/pdf" width="100%" height="500px">
       <p>Unable to display pdf <a href="Resume.pdf">Download Instead</a></p>
     </object>
   </>);
 };
-
+//note: github pages is static, thus can only work if resume is in same exact directory as the one calling it
 export default Home;
